21bc42617be24675ae717896878343dc13d60695
Eunseo Choi <yses9296@gmail.com>
2022-07-15 23:36:03 +0900

    2022-07-15

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Hello React This is</h1>
+        <h1>Hello React This is New World</h1>
+        <h2>{this.state.name}</h2>
+
+        <button onClick={ () => {this.setState({name: 'Eunseo'})}}>
+          Click Me
+        </button>
       </div>
     )
   }
