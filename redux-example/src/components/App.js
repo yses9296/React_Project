7d0ac7255d4100a2a6ba8d86817c9d4301265081
Eunseo Choi <yses9296@gmail.com>
2022-07-20 21:37:32 +0900

    2022-07-20

---
+++ redux-example/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+
+};
+const defaultProps = {
+
+};
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+  };
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
+export default App;
