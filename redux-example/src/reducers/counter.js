ef5e3076078ba22acf66e43be064f273615f29b9
Eunseo Choi <yses9296@gmail.com>
2022-07-21 16:49:30 +0900

    2022-07-21

---
+++ redux-example/src/reducers/counter.js
@@ -0,0 +1,20 @@
+import * as types from '../actions/ActionTypes';
+
+const initialState = {
+    number: 0
+};
+
+export default function counter(state = initialState, action){
+    // if( typeof state === 'undefined'){
+    //     return initialState;
+    // }
+
+    switch(action.type){
+        case types.INCREMENT:
+            return { ...state, number: state.number + 1} //...state: 복제 값
+        case types.DECREMENT:
+            return { ...state, number: state.number - 1}
+    }
+
+    return state;
+}
