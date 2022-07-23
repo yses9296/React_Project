c9ca78a41d0fbca046213d0a944d55ad3a3f465a
Eunseo Choi <yses9296@gmail.com>
2022-07-23 20:26:30 +0900

    2022-07-23

--- express-tutorial/main.js
+++ express-tutorial/main.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
-var user = require('./routes/user')
+
+var user = require('./routes/user');
+
+var morgan = require('morgan');
+
+// var myLogger = function(req, res, next){
+//     console.log(req.url);
+//     next();
+// };
+
+// app.use(myLogger);
+
+app.use(morgan('dev'));
 
 app.get('/', function(req, res){
     res.send('Hello World');
